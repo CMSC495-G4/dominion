9b9cfff7d9585953b44990eb18f2eca53d173533
Brian Park <park-brian@users.noreply.github.com>
2018-04-10 23:07:16 -0400

    create placeholder views

--- src/core/urls.py
+++ src/core/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('rules/', views.rules, name='rules'),
     path('about/', views.about, name='about'),
     path('signup/', views.signup, name='signup'),
+    path('profile/', views.profile, name='profile'),
+    path('history/', views.history, name='history'),
+    path('play/', views.play, name='play'),
 ]
