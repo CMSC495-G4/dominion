e0d9dde55a6f8cba5882930b8b50c5aa6f460b08
Brian Park <park-brian@users.noreply.github.com>
2018-04-11 01:50:29 -0400

    update deployment for linux

--- deploy/run.sh
+++ deploy/run.sh
@@ -6,4 +6,4 @@ docker build -t dominion -f deploy/Dockerfile .
 
 docker rm -f dominion-app
 
-docker run -dp 8000:80 -v "$pwd:/code" --name dominion-app dominion
+docker run -dp 80:80 -v "$PWD:/code" --name dominion-app dominion
