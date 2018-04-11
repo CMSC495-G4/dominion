74ac67b53a23a11ed59a5ac988dea870b02d6e04
Brian Park <park-brian@users.noreply.github.com>
2018-04-11 01:34:12 -0400

    properly serve static files

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -8,9 +8,10 @@ python3.6 manage.py collectstatic --noinput
 
 chown -R apache:apache /deploy
 
+
 mod_wsgi-express start-server dominion/wsgi.py \
   --user apache \
   --group apache \
   --port 80 \
   --reload-on-changes \
-  --url-alias /static /tmp
+  --url-alias /static static/ ## need trailing slash for directory
