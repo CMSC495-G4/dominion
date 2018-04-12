256284d752231d0932ae46b819748d3a005255e8
Brian Park <park-brian@users.noreply.github.com>
2018-04-12 02:05:00 -0400

    update deployment script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,7 +1,18 @@
 #!/bin/bash -ex
 
 KEY_PATH=/tmp/key
+REMOTE_PATH=/home/$REMOTE_USER/dominion
+
 echo -e $REMOTE_KEY > $KEY_PATH
 chmod 700 $KEY_PATH
-scp -q -r -o "StrictHostKeyChecking no" -i $KEY_PATH .:$REMOTE_USER@$REMOTE_HOSTNAME:/home/$REMOTE_USER/dominion
-ssh -q -o "StrictHostKeyChecking no" -i $KEY_PATH $REMOTE_USER@$REMOTE_HOSTNAME ls -l /home/$REMOTE_USER/dominion
+
+ssh -q -o "StrictHostKeyChecking no" -i $KEY_PATH $REMOTE_USER@$REMOTE_HOSTNAME bash <<EOF
+  rm -rf $REMOTE_PATH
+EOF
+
+scp -q -r -o "StrictHostKeyChecking no" -i $KEY_PATH . $REMOTE_USER@$REMOTE_HOSTNAME:$REMOTE_PATH
+ssh -q -o "StrictHostKeyChecking no" -i $KEY_PATH $REMOTE_USER@$REMOTE_HOSTNAME bash <<EOF
+  pushd $REMOTE_PATH/deploy
+  bash run.sh
+EOF
+
