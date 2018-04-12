7d2f8ffe94abe50d429fcf289df35d08157cce68
Brian Park <park-brian@users.noreply.github.com>
2018-04-12 01:48:58 -0400

    do not use strict host key checking

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,4 +2,4 @@
 
 KEY_PATH=/tmp/key
 echo -e $REMOTE_KEY > $KEY_PATH
-ssh -i $KEY_PATH $REMOTE_USER@$REMOTE_HOSTNAME ls -l
+ssh -o "StrictHostKeyChecking no" -i $KEY_PATH $REMOTE_USER@$REMOTE_HOSTNAME ls -l
